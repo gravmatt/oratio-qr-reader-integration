ab35020860a35f8d142049b31e85757e25730986
Rene Tanczos <gravmatt@gmail.com>
2017-09-27 14:37:52 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const qr = require('./external/node-qr-code-reader/qrCodeReader');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 8080;
+
+app.set('trust proxy', 1);
+app.set('x-powered-by', false);
+
+app.disable('etag');
+app.disable('view cache');
+
+app.use(bodyParser.json());
+
+app.get('/gravmatt', (req, res) => {
+    console.log(req.body);
+
+    res.send('nothing her yet');
+    // qr.decode('https://appcoda.com/wp-content/uploads/2013/12/qrcode.jpg')
+    //     .then(console.log)
+    //     .catch(console.log);
+});
+
+app.listen(process.env.GRAVMATT_PORT || PORT, () => {
+    log.info('Server is listening on port ' + (process.env.GRAVMATT_PORT || PORT));
+});
